d5310580e0941e3da9ca7c60c29efd17b827c40e
Nate_Optimus <nathanpwaters@outlook.com>
2018-05-02 20:46:24 +1000

    -part 1 --implement processing flows

--- a2_part_1.py
+++ a2_part_1.py
@@ -6,28 +6,42 @@ import subprocess
 import shlex
 from collections import OrderedDict
 
+
+def process_flow(f):
+    # TODO: docstring
+    # store our service output to use as input for the next service
+    service_output = ''
+    for service in flows.get(f):
+        result = subprocess.run(
+            shlex.split(
+                "%s %s" % (
+                    services.get(service)["program"],
+                    services.get(service)["parameters"])
+            ),
+            input=service_output,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        service_output = result.stdout
+
+
 # TODO: handle invalid json etc per specs
 configuration = json.load(
         open("test.json"),
         object_pairs_hook=OrderedDict
     )
 
-# execute the services
+# get the services
 services = configuration.get("services")
 
-# store our service output to use as input for the next service
-service_output = ''
+# get the flows
+flows = configuration.get("flows")
 
-for service_name, service_details in services.items():
-    result = subprocess.run(
-        shlex.split(
-            "%s %s" % (service_details["program"], service_details["parameters"])
-        ),
-        input=service_output,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    service_output = result.stdout
+
+# process the flows
+
+for flow, service_list in flows.items():
+    process_flow(flow)
     continue
 
 sys.exit()
