5c9e08cb27a7ccbeb8102608754a8379ae9e676e
Nate_Optimus <nathanpwaters@outlook.com>
2018-05-08 21:28:24 +1000

    -part 1 --refactored to use main() -part 2 --(wip) begin basic structure planning/implementation

--- a2_part_1.py
+++ a2_part_1.py
@@ -8,9 +8,43 @@ from collections import OrderedDict
 import time
 import datetime
 
-
-def get_configuration(
+# ------------------------------------------------------------------------------
+# part 2
+def verify_service_input_format(
         service,
+        service_input
+):
+    """
+    -service input will be the output of the preceding service in the flow
+    -input will be in the form of a JSON formatted string
+    :return:
+    """
+    # get the input type
+
+    # get the required format, if one exists
+
+
+
+# def get_configuration_part_2(
+#         service,
+#         service
+# ):
+#     pass
+#
+# def get_services_part_2():
+#     return services
+#
+# # ------------------------------------------------------------------------------
+#
+#
+# def get_services_part_1():
+#
+#     return services
+
+
+def get_configuration_part_1(
+        service,
+        services,
         service_output
 ):
     program = services.get(service)["program"]
@@ -27,7 +61,7 @@ def get_configuration(
     return command_line
 
 
-def process_flow(f):
+def process_flow(f, flows, services):
     # TODO: docstring
     # store our service output to use as input for the next service
     service_output = ''
@@ -35,8 +69,9 @@ def process_flow(f):
         # handle a possible exception if the service exits with a non-zero exit code
         try:
             result = subprocess.run(
-                get_configuration(
+                get_configuration_part_1(
                     service,
+                    services,
                     service_output
                 ),
                 input=service_output,
@@ -58,31 +93,55 @@ def process_flow(f):
     return True
 
 
-# TODO: handle invalid json etc per specs
-configuration = json.load(
+
+
+def main():
+    # get the program configuration
+    # TODO: handle invalid json etc per specs
+    configuration = json.load(
         open("test.json"),
         object_pairs_hook=OrderedDict
     )
+    # get the flow configuration
+    flows = configuration.get("flows")
 
-# get the services
-services = configuration.get("services")
+    # get the services
+    # part 1
+    services = configuration.get("services")
 
-# get the flows
-flows = configuration.get("flows")
+    # part 2
+    # services = get_services_part_2()
 
 
-# process the flows
+    # process the flows
 
-while True:
-    for flow, service_list in flows.items():
-        flow_status = ""
-        start_time = datetime.datetime.now()
-        if process_flow(flow):
-            flow_status = "successful!"
-        else:
-            flow_status = "failed!"
-        time_taken = datetime.datetime.now() - start_time
-        continue
-    time.sleep(1)
+    while True:
+        for flow, service_list in flows.items():
+            flow_status = ""
+            start_time = datetime.datetime.now()
+            if process_flow(flow, flows, services):
+                flow_status = "successful!"
+            else:
+                flow_status = "failed!"
+            time_taken = datetime.datetime.now() - start_time
+            continue
+        time.sleep(1)
 
-sys.exit()
+    sys.exit()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
