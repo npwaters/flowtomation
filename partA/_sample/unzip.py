cf395b98942a28d2bed5b63dc5a0fd3434b479d9
Nate_Optimus <nathanpwaters@outlook.com>
2018-06-20 20:51:29 +1000

    -partA sample (unzip) --fix style issues --fix indentation

--- partA/_sample/unzip.py
+++ partA/_sample/unzip.py
@@ -6,23 +6,17 @@
 import sys
 import zipfile
 
-def main():
-	lines = sys.stdin.read()	
-	lines = lines.split('\n')
-	lines = [line for line in lines if len(line) > 0]
 
-	for file in lines:
-		if zipfile.is_zipfile(file):
-			with zipfile.ZipFile(file) as myzip:
-				for name in myzip.namelist():
-					print(name)
+def main():
+    lines = sys.stdin.read()
+    lines = lines.split('\n')
+    lines = [line for line in lines if len(line) > 0]
+
+    for file in lines:
+        if zipfile.is_zipfile(file):
+            with zipfile.ZipFile(file) as myzip:
+                for name in myzip.namelist():
+                    print(name)
+
 
 main()
-
-#try:
-#	main()
-#	
-#except:
-#	sys.exit(1)
-#
-#sys.exit(0)
