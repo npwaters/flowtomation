5a4547adb8c702343e22789772df23d49d71c7d5
Nate_Optimus <nathanpwaters@outlook.com>
2018-06-20 20:46:50 +1000

    -partA sample (files) --fix style issues --fix indentation --fix exception too broad issue

--- partA/_sample/files.py
+++ partA/_sample/files.py
@@ -3,15 +3,16 @@
 import sys
 import os
 
+
 def main(directory):
-  files = os.listdir(directory)	
-  for file in files:
-    print("{0}/{1}".format(directory, file))
-		
+    files = os.listdir(directory)
+    for file in files:
+        print("{0}/{1}".format(directory, file))
+
 
 try:
-  main(sys.argv[1])
-except:
-  sys.exit(1)
+    main(sys.argv[1])
+except IndexError:
+    sys.exit(1)
 
 sys.exit(0)
