1a9f190b60e2709a0d65c9dfcc6e1c15e5ada26b
Nate_Optimus <nathanpwaters@outlook.com>
2018-06-20 20:26:30 +1000

    -part A sample (append) --improve style --fix indentation --fix exception too broad issue

--- partA/_sample/append.py
+++ partA/_sample/append.py
@@ -2,22 +2,22 @@
 
 import sys
 
-def main(save_name):
-	lines = sys.stdin.read()	
-	lines = lines.split('\n')
-	lines = [line for line in lines if len(line) > 0]
 
-	output = open(save_name, 'a')
-	# output.writelines(lines)
-	for line in lines:
-		output.write("{0}\n".format(line))
-		
-	output.close()
-	
+def main(save_name):
+    lines = sys.stdin.read()
+    lines = lines.split('\n')
+    lines = [line for line in lines if len(line) > 0]
+
+    output = open(save_name, 'a')
+    for line in lines:
+        output.write("{0}\n".format(line))
+
+    output.close()
+
 
 try:
-	main(sys.argv[1])
-except:
-	sys.exit(1)
+    main(sys.argv[1])
+except IndexError:
+    sys.exit(1)
 
 sys.exit(0)
